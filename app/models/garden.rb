247bf7b3529fecfbea99bdcbb0557a0f9e361ff5
Mathias Thibaud <math.thibaud@gmail.com>
2019-12-09 16:44:44 +0100

    deleted regexp

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -2,5 +2,5 @@ class Garden < ApplicationRecord
   has_many :plants, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
-  validates :banner_url, presence: true, format: { with: /(https?:\/\/.*\.(?:png|jpg))/ }
+  validates :banner_url, presence: true
 end
