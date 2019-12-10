b4b76a1bca5f49f881d3ac8d2402155c69438b17
Mathias Thibaud <math.thibaud@gmail.com>
2019-12-10 11:36:32 +0100

    added url regexp and fixed bug in the show garden

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -2,5 +2,5 @@ class Garden < ApplicationRecord
   has_many :plants, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
-  validates :banner_url, presence: true
+  validates :banner_url, presence: true, format: { with: URI::regexp(%w(http https)), message: "must be a valid image url" }
 end
